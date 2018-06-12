2ac80bb4edf3a96c920b8dc4c943d54d183157a9
Ramkumar Athinarayanan <-adm-athir0-ams@Bp1azutt.ad.bpazure.com>
2018-06-12 10:14:56 +0000

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=test321-aee7-Web-Deploy.pubxml -p:Password=e8W4TQDFbwAAasznpYeuEMCu1x1qhmMMuMGWo4x7T1MrcQkFcfDrZTJKJHMj
+
